b362aa187788127d8c4b4e899a4b68406f951d32
Zaki Tewari <ztewari@amok.cs.ucl.ac.uk>
2024-01-07 22:33:00 +0000

    when approach initiated

--- CreateDataset.py
+++ CreateDataset.py
@@ -6,12 +6,11 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf
 
 # for shared metastore (shared across all users)
-spark = SparkSession.builder.appName("Building dataset").config("hive.metastore.uris", "thrift://bialobog:9083", conf=SparkConf()).getOrCreate() \
+spark = SparkSession.builder.appName("Building dataset").config("hive.metastore.uris", "thrift://amok:9083", conf=SparkConf()).getOrCreate() \
 
 # for local metastore (your private, invidivual database) add the following config to spark session
-spark.sql("USE 2023_04_01")
+spark.sql("USE 2023_11_02")
 
-import pyspark.pandas as ps
 from pyspark.sql.functions import lit,col
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -38,55 +37,64 @@ import os
 
 def get_fund_data(imp_df):
     imp_df.createOrReplaceTempView("temp_table")
-    query2 = f"""SELECT t.fsym_id, p.p_date, p.p_price, splits.p_split_date, splits.p_split_factor, 
-                divs.p_divs_exdate, divs.p_divs_s_pd, divs.p_divs_pd, ms.p_com_shs_out 
+    query = f"""SELECT t.fsym_id, p.p_date AS date, p.p_price AS price , splits.p_split_date,
+    IF(ISNULL(splits.p_split_factor),1,splits.p_split_factor) AS split_factor
                 FROM temp_table t
-                LEFT JOIN sym_ticker_region s ON s.fsym_id = t.fsym_id
-                LEFT JOIN FF_SEC_COVERAGE c ON c.fsym_id = s.fsym_id
-                LEFT JOIN sym_coverage sc ON sc.fsym_id = s.fsym_id
+                LEFT JOIN FF_SEC_COVERAGE c ON c.fsym_id = t.fsym_id
+                LEFT JOIN sym_coverage sc ON sc.fsym_id = t.fsym_id
                 INNER JOIN fp_basic_prices p ON p.fsym_id = sc.fsym_regional_id
-                LEFT JOIN fp_basic_dividends divs ON divs.p_divs_exdate = p.p_date AND p.fsym_id = divs.fsym_id
                 LEFT JOIN fp_basic_splits AS splits ON splits.p_split_date = p.p_date AND p.fsym_id = splits.fsym_id
-                LEFT JOIN (SELECT sf.fsym_id, mp.price_date, sf.p_com_shs_out, sf.p_date AS shares_hist_date
-                                        FROM fp_basic_shares_hist AS sf
-                                        JOIN (SELECT p2.fsym_id, p2.p_date AS price_date, max(h.p_date) AS max_shares_hist_date
-                                                FROM fp_basic_prices AS p2
-                                                JOIN fp_basic_shares_hist AS h ON h.p_date <= p2.p_date AND p2.fsym_id = h.fsym_id
-                                                GROUP BY p2.fsym_id, p2.p_date)
-                                        mp ON mp.fsym_id = sf.fsym_id AND mp.max_shares_hist_date = sf.p_date)
-                            ms ON ms.fsym_id = p.fsym_id AND ms.price_date = p.p_date
+                LEFT JOIN fp_basic_dividends AS divs ON divs.p_divs_exdate = p.p_date AND p.fsym_id = divs.fsym_id
                 WHERE p.p_date >= '2001-01-01'
-                ORDER BY s.fsym_id, p.p_date
-                """
-    adj = spark.sql(query2)
-    imp_df = imp_df.toPandas()
-    adj = adj.withColumn("temp_cum_split_factor", F.when(adj.p_date==adj.p_split_date, F.lit(adj.p_split_factor)).otherwise(F.lit(1.0)))
-    adj = adj.withColumn("cum_split_factor", F.lit(0.0))
+                ORDER BY t.fsym_id, p.p_date"""
 
-    window_spec = Window.partitionBy('fsym_id').orderBy(F.desc('p_date'))
-
-    adj = adj.withColumn('cum_split_factor_no_lag', 
-                        F.exp(F.sum(F.log('temp_cum_split_factor')).over(window_spec)))
-
-    adj = adj.withColumn('cum_split_factor', 
-                        F.when(F.row_number().over(window_spec) == 1, 1.0)
-                        .otherwise(F.lag('cum_split_factor_no_lag', default=1.0).over(window_spec)))
     
-    adj = adj.withColumn('split_adj_price', adj.p_price * adj.cum_split_factor)
-    
-    adj = adj.withColumn('Market_Value', col('split_adj_price') * col('p_com_shs_out'))
-    
-    adj = adj.withColumn('year', F.year('p_date'))
-    
-    adj =adj.withColumn('week_of_year', F.weekofyear('p_date'))
 
-    window_spec = Window.partitionBy('fsym_id', 'year', 'week_of_year').orderBy(col('p_date').desc())
+    adj = spark.sql(query)
+    div_query = """SELECT fsym_id, p_divs_exdate AS date, SUM(p_divs_pd) AS div FROM fp_basic_dividends 
+                    GROUP BY fsym_id, p_divs_exdate
+                    ORDER BY fsym_id, p_divs_exdate"""
+    df_div = spark.sql(div_query)
+    df_div = df_div.withColumn('weekday', F.dayofweek('date'))
+    dic = {1:2, 2:0, 3:0, 4:0, 5:0, 6:0, 7:1}
+    df_div = df_div.na.replace(dic, subset="weekday")
+    df_div = df_div.withColumn('date_adj', F.expr("date_sub(date, weekday)"))
+    df = adj.join(df_div, (adj.fsym_id == df_div.fsym_id) & (adj.date == df_div.date_adj), how='left') \
+          .select(adj.fsym_id, adj.date, adj.price, df_div.div, adj.split_factor) \
+          .na.fill(0, subset='div') \
+          .orderBy(F.col('fsym_id').asc(), F.col('date').desc())
+    window_spec = Window.partitionBy("fsym_id").orderBy(F.desc("date"))
 
-    adj = adj.withColumn('row_num', F.row_number().over(window_spec))
+    df = df.withColumn("cum_split", F.lag(F.exp(F.sum(F.log("split_factor")).over(window_spec))).over(window_spec))
+    df = df.na.fill(1, subset='cum_split') # Set cumulative split factor of latest date to 1
 
-    adj = adj.filter(col('row_num') == 1).orderBy('p_date')
-    adj = adj.drop('temp_cum_split_factor', 'cum_split_factor', 'cum_split_factor_no_lag', 'row_num', 'p_split_date', 'p_split_factor')
-    return adj
+    # Split-adjusted price and dividends
+    df = df.withColumn("price_split_adj", F.col('price') * F.col('cum_split'))
+    df = df.withColumn("div_split_adj", F.col('div') * F.col('cum_split'))
+
+    # Dividend factor
+    df = df.withColumn('div_factor', (F.col('price_split_adj') - F.lag('div_split_adj').over(window_spec))/F.col('price_split_adj'))
+    df = df.na.fill(1, subset='div_factor') # Set dividend factor of latest date to 1
+
+    # Cumulative dividend factor
+    df = df.withColumn("cum_div", F.exp(F.sum(F.log("div_factor")).over(window_spec)))
+    df = df.na.fill(1, subset='cum_div') # Set cumulative dividend factor of latest date to 1
+
+    # Price adjusted for splits and dividends
+    df = df.withColumn('adj_price', F.col('price_split_adj') * F.col('cum_div'))
+    df = df.orderBy('fsym_id','date')
+                         
+    df = df.withColumn('year', F.year('date'))
+    
+    df = df.withColumn('week_of_year', F.weekofyear('date'))
+
+    window_spec = Window.partitionBy('fsym_id', 'year', 'week_of_year').orderBy(col('date').desc())
+
+    df = df.withColumn('row_num', F.row_number().over(window_spec))
+
+    df = df.filter(col('row_num') == 1)
+    df = df.select('fsym_id', 'date', 'adj_price').orderBy('fsym_id','date')
+    return df
 
 
 def get_feature_col_names():
@@ -112,15 +120,14 @@ def get_not_null_cols(df, table='FF_ADVANCED_DER_AF'):
     q_df = spark.sql(query1)
     column_types = q_df.dtypes
 
-    # Collect column names that are not of type float
-    columns_to_drop = [col_name for col_name, col_type in column_types if col_type != 'double']
+    null_pcts = [F.sum(F.col(col_name).isNull().count()) / F.count("*") for col_name in q_df.columns]
 
-    # Drop columns that are not of type float
+    columns_to_drop = [col_name for col_name, null_pct in zip(q_df.columns, null_pcts) if null_pct > 0.2]
+
     q_df = q_df.drop(*columns_to_drop)
 
-    q_df = ps.DataFrame(q_df)
-    null_pcts = q_df.isnull().sum()/len(q_df)
-    cols = null_pcts[null_pcts <= 0.2].index.tolist()
+    cols = q_df.columns
+
     return cols
     
 
@@ -159,7 +166,7 @@ def get_full_series_stocks(imp_df_price):
     return fsym_ids_2001
 
 
-def get_features_all_stocks_seq(df):
+def get_features_all_stocks_seq(df, all_feats=False):
     orig_df = spark.createDataFrame(df)
     table = "FF_ADVANCED_DER_AF"
     def generate_dates():
@@ -179,7 +186,10 @@ def get_features_all_stocks_seq(df):
     
     spark_df = spark.createDataFrame(df)
     spark_df.createOrReplaceTempView("temp_table")
-    col_names = get_feature_col_names()
+    if all_feats:
+        col_names = get_not_null_cols(df)
+    else:
+        col_names = get_feature_col_names()
     col_string = ', '.join('a.' + item for item in col_names)
     q=f"""SELECT t.fsym_id, t.year, a.date, {col_string}
                 FROM temp_table t
@@ -275,7 +285,7 @@ def get_features_all_stocks_seq(df):
         *[F.expr(f"IF(size({col}) < 22, concat({col}, array_repeat(0, 22 - size({col}))), {col})").alias(col) for col 
           in feature_cols])
     
-    orig_df = orig_df.withColumn('label', F.when(F.isnan('Implosion_Start_Date'), 0).otherwise(1))
+    orig_df = orig_df.withColumn('label', F.when(F.isnull('Implosion_Start_Date'), 0).otherwise(1))
     joined_df = grouped_df_padded.join(orig_df.select("fsym_id", "label"), "fsym_id", "inner")
     joined_df=joined_df.orderBy('fsym_id')
 
@@ -286,9 +296,12 @@ def get_features_all_stocks_seq(df):
 
 
 
-def get_tabular_dataset(all_feats=False):
+def get_tabular_dataset(all_feats=False, imploded_only=False):
     table = "FF_ADVANCED_DER_AF"
     df = pd.read_csv('imploded_stocks_price.csv', index_col=False)
+    if imploded_only:
+        df = df[df['Implosion_Start_Date'].notnull()]
+        
     spark_df = spark.createDataFrame(df)
     spark_df.createOrReplaceTempView("temp_table")
     
